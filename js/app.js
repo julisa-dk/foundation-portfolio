7a6bcf88060ce4a01a0ff13914fe84beb55151db
julisa-dk <julisa.khalina@gmail.com>
2021-10-21 14:39:50 +0200

    portfolio deploy
    portfolio v.1

---
+++ js/app.js
@@ -0,0 +1,46 @@
+(function ($) {
+
+    'use strict';
+
+    // scrollspy
+    $(".navbar-vertical").scrollspy({
+        offset: 200
+    });
+
+    //mfp popup 
+    $('.mfp-image').magnificPopup({
+        type: 'image',
+        closeOnContentClick: true,
+        mainClass: 'mfp-fade',
+        gallery: {
+            enabled: true,
+            navigateByImgClick: true,
+            preload: [0, 1]
+        }
+    });
+
+    // animated header text
+    $('.tlt').textillate({
+        loop: true,
+        minDisplayTime: 2e3,
+        initialDelay: 0,
+        autoStart: true,
+        "in": {
+            effect: "flipInY",
+            delayScale: 2.5,
+            delay: 50,
+            sync: false,
+            shuffle: false,
+            reverse: false
+        },
+        out: {
+            effect: "flipOutY",
+            delayScale: 2.5,
+            delay: 50,
+            sync: false,
+            shuffle: false,
+            reverse: false
+        }
+    });
+
+})(jQuery)
